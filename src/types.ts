b795bec1130febccaae591ec0242b8133563a29f
Chris Lloyd <chris.lloyd.hi@gmail.com>
2024-11-28 06:53:15 +1300

    feat: add taskDescription to settings
    #15

--- src/types.ts
+++ src/types.ts
@@ -23,6 +23,7 @@ export interface Settings {
   harvestAccountId: string;
   forecastAccountId: string;
   task?: string;
+  taskDescription?: string;
   billable: HourType;
   timeFormat?: TimeFormat;
   timePeriod?: TimePeriod;
